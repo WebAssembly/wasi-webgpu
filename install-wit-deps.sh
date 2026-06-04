62138d068346ed4622be8b0e63c42bc8887efdca
MendyBerger <12537668+MendyBerger@users.noreply.github.com>
2026-06-04 12:11:53 -0400

    refactor!: remove everything that isn't directly wasi:webgpu

--- install-wit-deps.sh
+++
@@ -1,4 +0,0 @@
-wit-deps -m webgpu/deps.toml -l webgpu/deps.lock -d webgpu/deps update
-wit-deps -m surface/deps.toml -l surface/deps.lock -d surface/deps update
-wit-deps -m graphics-context/deps.toml -l graphics-context/deps.lock -d graphics-context/deps update
-wit-deps -m frame-buffer/deps.toml -l frame-buffer/deps.lock -d frame-buffer/deps update
